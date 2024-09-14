edbe947be1db950608e7b5e85a4a071b61bbbb7d
Brian Rahadi <brian.rahadi@gmail.com>
2024-09-14 10:20:45 -0400

    into components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,87 +1,13 @@
 import { useState, useEffect } from 'react'
 import logo from './logo.svg'
+import Timer from './components/Timer';
 import './App.css'
 
 function App() {
-  const [endTime, setEndTime] = useState(null);
-  const [startTime, setStartTime] = useState(null);
-  const [timeElapsed, setTimeElapsed] = useState(0);
-
-  const startTimer = async () => {
-    let startTime = null;
-    let endTime = null;
-    try {
-      const result = await chrome.storage.local.get(['startTime', 'endTime']);
-
-      startTime = result.startTime;
-      endTime = result.endTime;
-
-      if (!startTime || !endTime) {
-        startTime = Date.now();
-        endTime = startTime + 600000;
-        await chrome.storage.local.set({ startTime, endTime });
-      }
-
-      console.log('Start Time and End Time saved!');
-
-      // Update the states
-      setEndTime(endTime);
-      setStartTime(startTime);
-    } catch (error) {
-      console.error('Error fetching or saving time from storage:', error);
-    }
-  }
-
-  function stopTimer() {
-    chrome.storage.local.get(['endTime'], function (result) {
-      if (result.endTime) {
-        console.log("Timer Removed!")
-        chrome.storage.local.remove(['endTime']);
-      }
-    });
-    chrome.storage.local.get(['startTime'], function (result) {
-      if (result.startTime) {
-        console.log("Timer 2 Removed!")
-        chrome.storage.local.remove(['startTime']);
-      }
-    });
-  }
-
-  const formatTime = (elapsedTime) => {
-    if (!elapsedTime) {
-      return "...";
-    }
-    const minutes = Math.floor(elapsedTime / 60);
-    const seconds = elapsedTime % 60;
-
-    // Format minutes and seconds with leading zeroes if needed
-    const formattedMinutes = String(minutes).padStart(2, '0');
-    const formattedSeconds = String(seconds).padStart(2, '0');
-
-    return `${formattedMinutes}:${formattedSeconds}`;
-  };
-
-  useEffect(() => {
-    let interval = null;
-
-    if (startTime) {
-      interval = setInterval(() => {
-        setTimeElapsed(Math.floor((endTime - Date.now()) / 1000)); // Updates every second
-      }, 1000);
-    }
-
-    return () => clearInterval(interval);
-  }, [startTime]);
-
-  useEffect(() => {
-    startTimer();
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
-        <p>Pomodoro2 Time</p>
-        <h1>{formatTime(timeElapsed)}</h1>
+        <Timer />
       </header>
     </div>
   )
---
+++ client/src/components/Timer.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect, useRef} from 'react'
+
+function Timer() {
+    const [endTime, setEndTime] = useState(null);
+    const [startTime, setStartTime] = useState(null);
+    const [timeElapsed, setTimeElapsed] = useState(null);
+    const intervalRef = useRef(null); // Holds the interval ID
+
+    const startTimer = async () => {
+        let startTime = null;
+        let endTime = null;
+        try {
+            const result = await chrome.storage.local.get(['startTime', 'endTime']);
+
+            startTime = result.startTime;
+            endTime = result.endTime;
+
+            if (!startTime || !endTime) {
+                startTime = Date.now();
+                endTime = startTime + 600000;
+                await chrome.storage.local.set({ startTime, endTime });
+            }
+
+            setEndTime(endTime);
+            setStartTime(startTime);
+        } catch (error) {
+            console.error('Error fetching or saving time from storage:', error);
+        }
+    }
+
+    const stopTimer = async () => {
+        await chrome.storage.local.remove(['startTime', 'endTime'], function (result) {
+            console.log("Timer stopped!")
+        });
+        setTimeElapsed(0);
+        clearInterval(intervalRef.current);
+    }
+
+    const formatTime = (elapsedTime) => {
+        if (elapsedTime === null) {
+            return "...";
+        }
+        const minutes = Math.floor(elapsedTime / 60);
+        const seconds = elapsedTime % 60;
+
+        // Format minutes and seconds with leading zeroes if needed
+        const formattedMinutes = String(minutes).padStart(2, '0');
+        const formattedSeconds = String(seconds).padStart(2, '0');
+
+        return `${formattedMinutes}:${formattedSeconds}`;
+    };
+
+    useEffect(() => {
+        let interval = null;
+
+        if (startTime) {
+            intervalRef.current = setInterval(() => {
+                const remainingTime = Math.floor((endTime - Date.now()) / 1000);
+
+                if (remainingTime <= 0) {
+                    clearInterval(interval); // Stop the interval if the time has passed
+                    setTimeElapsed(0); // Optionally set timeElapsed to 0 if the time is up
+                } else {
+                    setTimeElapsed(remainingTime);
+                }
+            }, 1000);
+        }
+
+        return () => clearInterval(interval);
+    }, [startTime]);
+
+    useEffect(() => {
+        startTimer();
+    }, []);
+
+    return (
+        <div className="section">
+            <h2>Timer</h2>
+            <h1>{formatTime(timeElapsed)}</h1>
+            <div class="flex">
+                <button onClick={startTimer} disabled={timeElapsed !== 0}>Start Timer</button>
+                <button onClick={stopTimer} disabled={timeElapsed === 0}>Stop Timer</button>
+            </div>
+        </div>
+    )
+}
+
+export default Timer;
