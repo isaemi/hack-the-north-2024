a40e003b8b4cbb5ea752d77df1c56f84041e24d5
Justin <justinray188@outlook.com>
2024-09-14 15:57:13 -0400

    remove unused print

--- server/db.py
+++ server/db.py
@@ -8,4 +8,3 @@ MONGO_DETAILS = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 client = AsyncIOMotorClient(MONGO_DETAILS)
 database = client.quiz_db
 quiz_collection = database.get_collection("quizzes")
-print(quiz_collection)
