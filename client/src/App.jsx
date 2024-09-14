eb3fe20805fa88808c6b21ef71b5cd1976887808
Brian Rahadi <brian.rahadi@gmail.com>
2024-09-14 04:11:38 -0400

    Timer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [seconds, setSeconds] = useState(600);
+
+  const formatTime = (secondsTime) => {
+    const minutes = Math.floor(secondsTime / 60);
+    const seconds = secondsTime % 60;
+  
+    // Format minutes and seconds with leading zeroes if needed
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+  
+    return `${formattedMinutes}:${formattedSeconds}`;
+  };
+
+  useEffect(() => {
+    // Initialize the interval
+    const intervalId = setInterval(() => {
+      setSeconds((prevSeconds) => {
+        if (prevSeconds <= 0) {
+          clearInterval(intervalId);
+          return 0;
+        }
+        return prevSeconds - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <p>Pomodoro Time</p>
+        <h1>{formatTime(seconds)}</h1>
       </header>
     </div>
   )
